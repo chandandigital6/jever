5e3861d7f6c4052b521bbd477cdb01d1ce4e6cc8
tabrej03dz <tabrejalam770496@gmail.com>
2024-06-11 18:54:43 +0530

    some ui changes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,17 +1,17 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="" class="brand-link">
-        <img src="{{ asset('logo.jpg') }}" alt="AdminLTE Logo"
-             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Real Victory  </span>
-    </a>
+{{--    <a href="" class="brand-link">--}}
+{{--        <img src="{{ asset('logo.jpg') }}" alt="AdminLTE Logo"--}}
+{{--             class="brand-image img-circle elevation-3" style="opacity: .8">--}}
+{{--        <span class="brand-text font-weight-light">Real Victory  </span>--}}
+{{--    </a>--}}
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{asset('asset/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{asset('asset/images/jevarMahalLogo.png')}}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{ Auth::user()->name }}</a>
